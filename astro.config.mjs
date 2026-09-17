11959fb10601475bf0398f0f48a876f11f66dcee
Felipe Teles <felinjob@gmail.com>
2026-09-17 20:01:09 -0300

    build: shift to full static export bypassing vercel serverless functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
-import keystatic from '@keystatic/astro';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brunodourado.vercel.app',
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  integrations: [keystatic(), react(), sitemap()],
+  output: 'static',
+  integrations: [react(), sitemap()],
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
